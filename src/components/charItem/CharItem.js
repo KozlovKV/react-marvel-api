25d2ef8a1d7c4f98e979c31cbd6a78c79961e41e
Kirill Kozlov <kozlov.kirill.v.work@gmail.com>
2022-04-18 19:51:45 +0600

    Add simple toggle selection for char items in char list

--- src/components/charItem/CharItem.js
+++ src/components/charItem/CharItem.js
@@ -3,11 +3,20 @@ import { Component } from "react";
 import './charItem.scss';
 
 export default class CharItem extends Component {
+	state = {
+		active: false,
+	}
+
+	onToggleSelected = () => {
+		this.setState(({active}) => ({active: !active}));
+	} 
+
 	render() {
-		const { active, thumbnail, name } = this.props;
+		const { thumbnail, name } = this.props;
+		const { active } = this.state;
 		let itemClass = `char__item ${active ? 'char__item_selected' : ''}`;
 		return (
-			<li className={itemClass}>
+			<li className={itemClass} onClick={this.onToggleSelected}>
 				<img src={thumbnail.url} alt={name} style={thumbnail.style} />
 				<div className="char__name">{name}</div>
 			</li>
