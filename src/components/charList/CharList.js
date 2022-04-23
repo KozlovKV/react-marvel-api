8a976c7177ec51091d14cc2cad4a465cccec79c7
Kirill Kozlov <kozlov.kirill.v.work@gmail.com>
2022-04-24 01:31:39 +0600

    Update tab navigation in CharList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -18,12 +18,6 @@ export default class CharList extends Component {
 		this.loadCharacters();
 	}
 
-	onTABSelectChar = (e) => {
-		if (e.code === 'Tab' && e.target) {
-			this.props.onCharSelected(+e.target.dataset.id);
-		}
-	}
-
 	onCharactersLoaded = (newCharacters) => {
 		this.setState(({ characters }) => ({
 			characters: [...characters, ...newCharacters],
@@ -46,8 +40,8 @@ export default class CharList extends Component {
 				active = selectedCharId === id;
 			let itemClass = `char__item ${active ? 'char__item_selected' : ''}`;
 			return (
-				<li className={itemClass} tabIndex={i + 1}
-					onClick={() => onCharSelected(id)} key={i} data-id={id}>
+				<li className={itemClass} tabIndex="0"
+					onFocus={() => onCharSelected(id)} key={i} data-id={id}>
 					<img src={thumbnail.url} alt={name} style={thumbnail.style} />
 					<div className="char__name">{name}</div>
 				</li>
@@ -56,10 +50,10 @@ export default class CharList extends Component {
 	}
 
 	getLoadMoreButton() {
-		const canLoadMore = this.canLoadMore(), btnIndex = this.state.characters.length;
+		const canLoadMore = this.canLoadMore();
 		return <button className="button button__main button__long"
 			onClick={this.onLoadMore} disabled={!canLoadMore}
-			tabIndex={btnIndex}>
+			tabIndex="0">
 			<div className="inner">
 				{canLoadMore ? "load more" : "All characters loaded"}
 			</div>
@@ -80,7 +74,7 @@ export default class CharList extends Component {
 		const button = !loading ? this.getLoadMoreButton() : null;
 		return (
 			<div className="char__list">
-				<ul className="char__grid" onKeyUp={this.onTABSelectChar}>
+				<ul className="char__grid">
 					{this.getCharItems()}
 				</ul>
 				{spinner}
